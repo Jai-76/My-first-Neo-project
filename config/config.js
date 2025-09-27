83b4ade6fb579a36825851f8b65147c6d89d5bfd
Jai deep <jaishekhawat175@gmail.com>
2025-09-27 19:13:35 +0530

    Add Google Maps API key config

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoUri: 'mongodb://localhost:27017/neoride',
+    jwtSecret: 'yourJwtSecretKey',
+    port: 5000,
+    googleMapsApiKey: 'YOUR_VALID_API_KEY' // <-- Replace with your actual Google Maps API key
+};
